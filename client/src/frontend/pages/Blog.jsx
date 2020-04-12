d340592633f3a1a48031720464ebb631d6f10839
FrcGustavo <hidalgo_fco520@outlook.com>
2020-04-12 00:27:00 -0500

    Add page Blog with styles

--- client/src/frontend/pages/Blog.jsx
+++ client/src/frontend/pages/Blog.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Blog = () => (<h1>Blog!</h1>);
+import post from '../assets/img/post.png';
+
+const Blog = () => (
+  <div className="blog">
+    <div className="card-principle">
+      <div className="container">
+        <img src={post} alt="" />
+        <div className="card-info">
+          <h1>Titulo del post</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ut a fugiat soluta consequatur nisi sunt.</p>
+          <Link className="btn btn-primary" to="/">Leer más</Link>
+        </div>
+      </div>
+    </div>
+    <section className="gird-cards">
+      <div className="container">
+        <article className="card-post">
+          <img src={post} alt="" />
+          <div className="card-info">
+            <h1>Titulo del post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ut a fugiat soluta consequatur nisi sunt.</p>
+            <Link className="btn btn-primary" to="/">Leer más</Link>
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
+);
 
 export default Blog;
