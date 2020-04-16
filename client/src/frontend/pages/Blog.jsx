c5f50343846c6e767a05907616db94001772d989
FrcGustavo <hidalgo_fco520@outlook.com>
2020-04-15 21:39:10 -0500

    Add component Footer and fix errors

--- client/src/frontend/pages/Blog.jsx
+++ client/src/frontend/pages/Blog.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import CardPost from '../components/CardPost';
-
-const post = 'https://firebasestorage.googleapis.com/v0/b/frcgustavo-849f3.appspot.com/o/post.png?alt=media&token=01feb409-2585-4ffd-a9d1-11d84524f96d';
+import Footer from '../components/Footer';
 
 const Blog = () => {
   const [posts, setPosts] = useState(false);
@@ -12,30 +11,33 @@ const Blog = () => {
         .then((res) => res.json())
         .then((json) => setPosts(json.data));
     }
-    console.log(posts);
   });
-  console.log(posts);
+
   if (!posts) return <div>Cargando</div>;
+  const post = posts.shift();
   return (
-    <div className="blog">
-      <div className="card-principle">
-        <div className="container">
-          <img src={post} alt="" />
-          <div className="card-info">
-            <h1>Titulo del post</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ut a fugiat soluta consequatur nisi sunt.</p>
-            <Link className="btn btn-primary" to="/my-first-blog">Leer más</Link>
+    <>
+      <div className="blog">
+        <div className="card-principle">
+          <div className="container">
+            <img src={post.cover} alt="" />
+            <div className="card-info">
+              <h1>{post.title}</h1>
+              <p>{post.description}</p>
+              <Link className="btn btn-primary" to={post.slug}>Leer más</Link>
+            </div>
           </div>
         </div>
+        <section className="gird-cards">
+          <div className="container">
+            {
+              posts.map((post, idx) => (<CardPost key={idx} post={post} />))
+            }
+          </div>
+        </section>
       </div>
-      <section className="gird-cards">
-        <div className="container">
-          {
-            posts.map((post, idx) => (<CardPost key={idx} post={post} />))
-          }
-        </div>
-      </section>
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- client/src/frontend/pages/Post.jsx
+++ client/src/frontend/pages/Post.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import Footer from '../components/Footer';
 import { api } from '../config';
 
-const html = '<h2 className="subtitle-1">Subtituo 1</h2>\n<h3 className="subtitle-2">Subtitulo 2</h3>\n<p className="pharagraph">Parrafo de texto</p\n';
 const Post = (props) => {
   const [post, setPost] = useState(false);
   useEffect(() => {
@@ -12,20 +12,21 @@ const Post = (props) => {
     }
   });
   if (!post) return <div>Cargando</div>;
-  const content = post.post.split('\n');
-  console.log();
   return (
-    <div className="post">
-      <div className="container">
-        <section className="post-header">
-          <img src="https://firebasestorage.googleapis.com/v0/b/frcgustavo-849f3.appspot.com/o/post.png?alt=media&token=01feb409-2585-4ffd-a9d1-11d84524f96d" alt="" />
-        </section>
-        <section className="post-title">
-          <h1>{post.title}</h1>
-        </section>
-        <section className="post-conent" dangerouslySetInnerHTML={{ __html: post.post }} />
+    <>
+      <div className="post">
+        <div className="container">
+          <section className="post-header">
+            <img src="https://firebasestorage.googleapis.com/v0/b/frcgustavo-849f3.appspot.com/o/post.png?alt=media&token=01feb409-2585-4ffd-a9d1-11d84524f96d" alt="" />
+          </section>
+          <section className="post-title">
+            <h1>{post.title}</h1>
+          </section>
+          <section className="post-conent" dangerouslySetInnerHTML={{ __html: post.post }} />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
